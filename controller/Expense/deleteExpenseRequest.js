b1c5e4443900f86f6afe59e0b1e3ee4d52b9ec48
tolujohnson24 <127182885+tolujohnson24@users.noreply.github.com>
2026-02-11 08:47:56 +0100

    update

--- controller/Expense/deleteExpenseRequest.js
+++ controller/Expense/deleteExpenseRequest.js
@@ -1,143 +1,328 @@
 
-import dotenv from 'dotenv';
-import Department from '../../model/ExpenseRequests';
-import Employee from '../../model/Employees';
+// import dotenv from 'dotenv';
+// import Department from '../../model/ExpenseRequests';
+// import Employee from '../../model/Employees';
 
-import utils from '../../config/utils';
+// import utils from '../../config/utils';
 
-import { emailTemp } from '../../emailTemplate';
-
-
-const sgMail = require('@sendgrid/mail')
-
-dotenv.config();
+// import { emailTemp } from '../../emailTemplate';
 
 
+// const sgMail = require('@sendgrid/mail')
 
-
-sgMail.setApiKey(process.env.SENDGRID_KEY);
+// dotenv.config();
 
 
 
-const deleteExpenseRequest = async (req, res) => {
 
-    try {
-
-        const department = await Department.findOne({_id: req.params.id})
-        const employee = await Employee.findOne({_id: department.employeeId})
+// sgMail.setApiKey(process.env.SENDGRID_KEY);
 
 
-        console.log(department)
-        if(department.length < 1){
-            res.status(404).json({
-                status:404,
-                success: false,
-                error:'No Expense Request Found'
-            })
-            return
-        }else{
-            Department.remove({ _id: req.params.id, status: 'Pending'},
-                function (
-                    err,
-                    result
-                ) {
+
+// const deleteExpenseRequest = async (req, res) => {
+
+//     try {
+
+//         const department = await Department.findOne({_id: req.params.id})
+//         const employee = await Employee.findOne({_id: department.employeeId})
+
+
+//         console.log(department)
+//         if(department.length < 1){
+//             res.status(404).json({
+//                 status:404,
+//                 success: false,
+//                 error:'No Expense Request Found'
+//             })
+//             return
+//         }else{
+//             Department.remove({ _id: req.params.id, status: 'Pending'},
+//                 function (
+//                     err,
+//                     result
+//                 ) {
     
-                    console.log(result)
+//                     console.log(result)
     
-                    if (err) {
-                        res.status(401).json({
-                            status: 401,
-                            success: false,
-                            error: err
-                        })
-                    }
-                    else {
+//                     if (err) {
+//                         res.status(401).json({
+//                             status: 401,
+//                             success: false,
+//                             error: err
+//                         })
+//                     }
+//                     else {
 
-                        Employee.findOneAndUpdate(
-                            { _id: department.employeeId },
-                            {
-                              $set: {
-                                "expenseDetails.cardBalance":
-                                  Number(employee.expenseDetails.cardBalance) + Number(department.amount),
-                                "expenseDetails.totalSpent":
-                                  Number(employee.expenseDetails.totalSpent) - Number(department.amount),
-                                "expenseDetails.currentSpent": Number(department.amount),
-                                "expenseDetails.currentExpense":
-                                  Number(employee.expenseDetails.currentExpense) - Number(department.amount),
-                              },
-                            },
-                            async function (err, result) {
-                              if (err) {
-                                res.status(401).json({
-                                  status: 401,
-                                  success: false,
-                                  error: err,
-                                });
+//                         Employee.findOneAndUpdate(
+//                             { _id: department.employeeId },
+//                             {
+//                               $set: {
+//                                 "expenseDetails.cardBalance":
+//                                   Number(employee.expenseDetails.cardBalance) + Number(department.amount),
+//                                 "expenseDetails.totalSpent":
+//                                   Number(employee.expenseDetails.totalSpent) - Number(department.amount),
+//                                 "expenseDetails.currentSpent": Number(department.amount),
+//                                 "expenseDetails.currentExpense":
+//                                   Number(employee.expenseDetails.currentExpense) - Number(department.amount),
+//                               },
+//                             },
+//                             async function (err, result) {
+//                               if (err) {
+//                                 res.status(401).json({
+//                                   status: 401,
+//                                   success: false,
+//                                   error: err,
+//                                 });
                   
-                                return;
-                              } else {
-                                // const history = await Employee.findOneAndUpdate(
-                                //   { _id: req.payload.id },
-                                //   {
-                                //     $push: {
-                                //       "expenseDetails.expenseHistory": {
+//                                 return;
+//                               } else {
+//                                 // const history = await Employee.findOneAndUpdate(
+//                                 //   { _id: req.payload.id },
+//                                 //   {
+//                                 //     $push: {
+//                                 //       "expenseDetails.expenseHistory": {
                   
-                                //           employeeId: req.payload.id,
-                                //           companyId: employee.companyId,
-                                //           companyName: employee.companyName,
-                                //           expenseTypeId,
-                                //           expenseTypeName: expense.expenseCardName,
-                                //           expenseDate,
-                                //           attachment: image,
-                                //           approver: approve[0].approval,
-                                //           approverId: approve[0].approvalId,
-                                //           amount,
-                                //           image,
-                                //           description,
-                                //       },
-                                //     },
-                                //   }
-                                // );
+//                                 //           employeeId: req.payload.id,
+//                                 //           companyId: employee.companyId,
+//                                 //           companyName: employee.companyName,
+//                                 //           expenseTypeId,
+//                                 //           expenseTypeName: expense.expenseCardName,
+//                                 //           expenseDate,
+//                                 //           attachment: image,
+//                                 //           approver: approve[0].approval,
+//                                 //           approverId: approve[0].approvalId,
+//                                 //           amount,
+//                                 //           image,
+//                                 //           description,
+//                                 //       },
+//                                 //     },
+//                                 //   }
+//                                 // );
                   
                                 
                   
-                                // console.log({ history });
-                                // if (history) {
-                                //   res.status(200).json({
-                                //     status: 200,
-                                //     success: true,
-                                //     data: history
-                                //   });
-                                //   return;
-                                // } else {
-                                //   res.status(400).json({
-                                //     status: 400,
-                                //     error: "error saving expense history",
-                                //   });
-                                //   return;
-                                // }
-                              }
-                            }
-                          );
-                        res.status(200).json({
-                            status: 200,
-                            success: true,
-                            data: "Expense Request Deleted successfully!"
-                        })
-                    }
+//                                 // console.log({ history });
+//                                 // if (history) {
+//                                 //   res.status(200).json({
+//                                 //     status: 200,
+//                                 //     success: true,
+//                                 //     data: history
+//                                 //   });
+//                                 //   return;
+//                                 // } else {
+//                                 //   res.status(400).json({
+//                                 //     status: 400,
+//                                 //     error: "error saving expense history",
+//                                 //   });
+//                                 //   return;
+//                                 // }
+//                               }
+//                             }
+//                           );
+//                         res.status(200).json({
+//                             status: 200,
+//                             success: true,
+//                             data: "Expense Request Deleted successfully!"
+//                         })
+//                     }
     
-                })
+//                 })
+//         }
+
+//     } catch (error) {
+//         res.status(500).json({
+//             status: 500,
+//             success: false,
+//             error: error
+//         })
+//     }
+// }
+// export default deleteExpenseRequest;
+
+
+import dotenv from 'dotenv';
+import ExpenseRequests from '../../model/ExpenseRequests';
+import Employee from '../../model/Employees';
+import Company from '../../model/Company';
+
+dotenv.config();
+
+/**
+ * Delete an expense request (only if status is Pending)
+ * Refunds the amount back to employee's card balance
+ * @route DELETE /api/expense/request/:id
+ */
+const deleteExpenseRequest = async (req, res) => {
+    try {
+        const expenseId = req.params.id;
+        const userId = req.payload.id;
+
+        // Validate expense ID
+        if (!expenseId) {
+            return res.status(400).json({
+                status: 400,
+                success: false,
+                error: 'Expense request ID is required'
+            });
         }
 
+        // Check if user is company or authorized employee
+        const [company, employee] = await Promise.all([
+            Company.findById(userId).lean(),
+            Employee.findById(userId).lean()
+        ]);
+
+        let userCompany;
+        let isAuthorized = false;
+
+        if (company) {
+            userCompany = company;
+            isAuthorized = true;
+        } else if (employee) {
+            userCompany = await Company.findById(employee.companyId).lean();
+            
+            if (!userCompany) {
+                return res.status(404).json({
+                    status: 404,
+                    success: false,
+                    error: 'Company not found'
+                });
+            }
+
+            // Authorization check - can delete own requests or admin can delete any
+            isAuthorized = 
+                employee.isSuperAdmin === true ||
+                employee.role === 'Admin' ||
+                employee.roleName === 'Admin' ||
+                employee.companyRole === 'Admin' ||
+                employee.permissions?.expenseManagement?.delete_expense_request === true;
+        } else {
+            return res.status(404).json({
+                status: 404,
+                success: false,
+                error: 'User not found'
+            });
+        }
+
+        // Find the expense request
+        const expenseRequest = await ExpenseRequests.findOne({
+            _id: expenseId,
+            companyId: userCompany._id.toString()
+        });
+
+        if (!expenseRequest) {
+            return res.status(404).json({
+                status: 404,
+                success: false,
+                error: 'Expense request not found or does not belong to your company'
+            });
+        }
+
+        // Check if expense belongs to user (if not admin)
+        if (!isAuthorized && expenseRequest.employeeId.toString() !== userId) {
+            return res.status(403).json({
+                status: 403,
+                success: false,
+                error: 'You can only delete your own expense requests'
+            });
+        }
+
+        // Can only delete pending requests
+        if (expenseRequest.status !== 'Pending') {
+            return res.status(400).json({
+                status: 400,
+                success: false,
+                error: `Cannot delete expense request with status: ${expenseRequest.status}. Only pending requests can be deleted.`
+            });
+        }
+
+        // Get employee details for refund
+        const expenseEmployee = await Employee.findById(expenseRequest.employeeId);
+
+        if (!expenseEmployee) {
+            return res.status(404).json({
+                status: 404,
+                success: false,
+                error: 'Employee associated with this expense not found'
+            });
+        }
+
+        const expenseAmount = Number(expenseRequest.amount) || 0;
+
+        // Calculate updated balances
+        const currentCardBalance = Number(expenseEmployee.expenseDetails?.cardBalance) || 0;
+        const currentTotalSpent = Number(expenseEmployee.expenseDetails?.totalSpent) || 0;
+        const currentExpense = Number(expenseEmployee.expenseDetails?.currentExpense) || 0;
+
+        const updatedCardBalance = currentCardBalance + expenseAmount;
+        const updatedTotalSpent = Math.max(0, currentTotalSpent - expenseAmount);
+        const updatedCurrentExpense = Math.max(0, currentExpense - expenseAmount);
+
+        // Delete expense request and update employee in parallel
+        const [deletedExpense, updatedEmployee] = await Promise.all([
+            ExpenseRequests.findByIdAndDelete(expenseId),
+            Employee.findByIdAndUpdate(
+                expenseRequest.employeeId,
+                {
+                    $set: {
+                        'expenseDetails.cardBalance': updatedCardBalance,
+                        'expenseDetails.totalSpent': updatedTotalSpent,
+                        'expenseDetails.currentSpent': expenseAmount,
+                        'expenseDetails.currentExpense': updatedCurrentExpense
+                    }
+                },
+                { new: true }
+            ).select('expenseDetails firstName lastName fullName')
+        ]);
+
+        if (!deletedExpense) {
+            return res.status(500).json({
+                status: 500,
+                success: false,
+                error: 'Failed to delete expense request'
+            });
+        }
+
+        return res.status(200).json({
+            status: 200,
+            success: true,
+            message: 'Expense request deleted successfully',
+            data: {
+                deletedExpense: {
+                    id: deletedExpense._id,
+                    amount: expenseAmount,
+                    description: deletedExpense.description,
+                    expenseDate: deletedExpense.expenseDate
+                },
+                refund: {
+                    employeeName: updatedEmployee.fullName || `${updatedEmployee.firstName} ${updatedEmployee.lastName}`,
+                    refundedAmount: expenseAmount,
+                    previousBalance: currentCardBalance,
+                    newBalance: updatedCardBalance,
+                    previousTotalSpent: currentTotalSpent,
+                    newTotalSpent: updatedTotalSpent
+                }
+            }
+        });
+
     } catch (error) {
-        res.status(500).json({
+        console.error('[DeleteExpenseRequest] Error:', error);
+
+        if (error.name === 'CastError') {
+            return res.status(400).json({
+                status: 400,
+                success: false,
+                error: 'Invalid ID format'
+            });
+        }
+
+        return res.status(500).json({
             status: 500,
             success: false,
-            error: error
-        })
+            error: 'Internal server error',
+            message: process.env.NODE_ENV === 'development' ? error.message : 'An error occurred while deleting the expense request'
+        });
     }
-}
+};
+
 export default deleteExpenseRequest;
-
-
-
--- controller/Payroll/deletePeriod.js
+++ controller/Payroll/deletePeriod.js
@@ -1,82 +1,265 @@
 
+// import dotenv from 'dotenv';
+// import Role from '../../model/Role';
+// import Company from '../../model/Company';
+// import Leave from '../../model/Expense';
+// import FinalRating from '../../model/FinalRating';
+// import Period from '../../model/AppraisalPeriod'
+// import PayrollPeriod from '../../model/PayrollPeriod';
+
+
+
+
+// const sgMail = require('@sendgrid/mail')
+
+// dotenv.config();
+
+
+
+
+// sgMail.setApiKey(process.env.SENDGRID_KEY);
+
+
+
+// const deletePayrollPeriod = async (req, res) => {
+
+//     try {
+       
+          
+//         if (!req.params.id) {
+//             res.status(400).json({
+//                 status: 400,
+//                 error: 'Id is required'
+//             })
+//             return;
+//         }
+
+
+//         let leave = await PayrollPeriod.findOne({ _id: req.params.id });
+
+
+//         if (!leave) {
+//             res.status(400).json({
+//                 status: 400,
+//                 error: 'Payroll period not found'
+//             })
+//             return;
+//         }
+
+
+//         PayrollPeriod.remove({ _id: req.params.id},
+//             function (
+//                 err,
+//                 result
+//             ) {
+//                 if (err) {
+//                     res.status(401).json({
+//                         status: 401,
+//                         success: false,
+//                         error: err
+
+//                     })
+
+//                 } else {
+//                     res.status(200).json({
+//                         status: 200,
+//                         success: true,
+//                         data: "Deleted Successfully"
+//                     })
+
+//                 }
+//             })
+
+
+//     } catch (error) {
+//         res.status(500).json({
+//             status: 500,
+//             success: false,
+//             error: error
+//         })
+//     }
+// }
+// export default deletePayrollPeriod;
+
+
 import dotenv from 'dotenv';
-import Role from '../../model/Role';
-import Company from '../../model/Company';
-import Leave from '../../model/Expense';
-import FinalRating from '../../model/FinalRating';
-import Period from '../../model/AppraisalPeriod'
 import PayrollPeriod from '../../model/PayrollPeriod';
-
-
-
-
-const sgMail = require('@sendgrid/mail')
+import PeriodPayData from '../../model/PeriodPayData';
+import Company from '../../model/Company';
+import Employee from '../../model/Employees';
 
 dotenv.config();
 
-
-
-
-sgMail.setApiKey(process.env.SENDGRID_KEY);
-
-
-
+/**
+ * Delete a payroll period and all associated employee pay data
+ * Can only delete periods that are not approved or disbursed
+ * @route DELETE /api/payroll/period/:id
+ */
 const deletePayrollPeriod = async (req, res) => {
-
     try {
-       
-          
-        if (!req.params.id) {
-            res.status(400).json({
+        const periodId = req.params.id;
+        const userId = req.payload.id;
+
+        // Validate period ID
+        if (!periodId) {
+            return res.status(400).json({
                 status: 400,
-                error: 'Id is required'
-            })
-            return;
+                success: false,
+                error: 'Payroll period ID is required'
+            });
         }
 
+        // Check if user is company or authorized employee
+        const [company, employee] = await Promise.all([
+            Company.findById(userId).lean(),
+            Employee.findById(userId).lean()
+        ]);
 
-        let leave = await PayrollPeriod.findOne({ _id: req.params.id });
+        let userCompany;
+        let isAuthorized = false;
 
+        if (company) {
+            userCompany = company;
+            isAuthorized = true;
+        } else if (employee) {
+            userCompany = await Company.findById(employee.companyId).lean();
+            
+            if (!userCompany) {
+                return res.status(404).json({
+                    status: 404,
+                    success: false,
+                    error: 'Company not found'
+                });
+            }
 
-        if (!leave) {
-            res.status(400).json({
-                status: 400,
-                error: 'Payroll period not found'
-            })
-            return;
+            // Check authorization - only admins can delete payroll periods
+            isAuthorized = 
+                employee.isSuperAdmin === true ||
+                employee.role === 'Admin' ||
+                employee.roleName === 'Admin' ||
+                employee.companyRole === 'Admin' ||
+                employee.permissions?.payrollManagement?.delete_payroll_period === true;
+
+            if (!isAuthorized) {
+                return res.status(403).json({
+                    status: 403,
+                    success: false,
+                    error: 'You do not have permission to delete payroll periods. Only admins can perform this action.'
+                });
+            }
+        } else {
+            return res.status(404).json({
+                status: 404,
+                success: false,
+                error: 'User not found'
+            });
         }
 
+        // Find the payroll period
+        const payrollPeriod = await PayrollPeriod.findOne({
+            _id: periodId,
+            companyId: userCompany._id.toString()
+        });
 
-        PayrollPeriod.remove({ _id: req.params.id},
-            function (
-                err,
-                result
-            ) {
-                if (err) {
-                    res.status(401).json({
-                        status: 401,
-                        success: false,
-                        error: err
+        if (!payrollPeriod) {
+            return res.status(404).json({
+                status: 404,
+                success: false,
+                error: 'Payroll period not found or does not belong to your company'
+            });
+        }
 
-                    })
-
-                } else {
-                    res.status(200).json({
-                        status: 200,
-                        success: true,
-                        data: "Deleted Successfully"
-                    })
-
+        // Check if period is approved or disbursed
+        if (payrollPeriod.periodStatusApproved) {
+            return res.status(400).json({
+                status: 400,
+                success: false,
+                error: 'Cannot delete an approved payroll period. Please revoke approval first.',
+                periodStatus: {
+                    approved: payrollPeriod.periodStatusApproved,
+                    disbursed: payrollPeriod.periodPayrollDisbursed,
+                    status: payrollPeriod.status
                 }
-            })
+            });
+        }
 
+        if (payrollPeriod.periodPayrollDisbursed) {
+            return res.status(400).json({
+                status: 400,
+                success: false,
+                error: 'Cannot delete a disbursed payroll period.',
+                periodStatus: {
+                    approved: payrollPeriod.periodStatusApproved,
+                    disbursed: payrollPeriod.periodPayrollDisbursed,
+                    status: payrollPeriod.status
+                }
+            });
+        }
+
+        // Count associated employee pay data
+        const associatedPayDataCount = await PeriodPayData.countDocuments({
+            payrollPeriodId: periodId,
+            companyId: userCompany._id.toString()
+        });
+
+        // Delete payroll period and all associated employee pay data
+        const [deletedPeriod, deletedPayDataResult] = await Promise.all([
+            PayrollPeriod.findByIdAndDelete(periodId),
+            PeriodPayData.deleteMany({
+                payrollPeriodId: periodId,
+                companyId: userCompany._id.toString()
+            })
+        ]);
+
+        if (!deletedPeriod) {
+            return res.status(500).json({
+                status: 500,
+                success: false,
+                error: 'Failed to delete payroll period'
+            });
+        }
+
+        return res.status(200).json({
+            status: 200,
+            success: true,
+            message: 'Payroll period deleted successfully',
+            data: {
+                deletedPeriod: {
+                    id: deletedPeriod._id,
+                    name: deletedPeriod.payrollPeriodName,
+                    reference: deletedPeriod.reference,
+                    startDate: deletedPeriod.startDate,
+                    endDate: deletedPeriod.endDate,
+                    totalEarnings: deletedPeriod.totalEarnings,
+                    netEarnings: deletedPeriod.netEarnings
+                },
+                deletedEmployeeRecords: {
+                    count: deletedPayDataResult.deletedCount,
+                    expected: associatedPayDataCount
+                },
+                deletedBy: company ? company.companyName : employee?.fullName,
+                deletedAt: new Date()
+            }
+        });
 
     } catch (error) {
-        res.status(500).json({
+        console.error('[DeletePayrollPeriod] Error:', error);
+
+        if (error.name === 'CastError') {
+            return res.status(400).json({
+                status: 400,
+                success: false,
+                error: 'Invalid ID format'
+            });
+        }
+
+        return res.status(500).json({
             status: 500,
             success: false,
-            error: error
-        })
+            error: 'Internal server error',
+            message: process.env.NODE_ENV === 'development' ? error.message : 'An error occurred while deleting the payroll period'
+        });
     }
-}
+};
+
 export default deletePayrollPeriod;
